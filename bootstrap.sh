aad2155beb7cd85b8a615eb0f2633b55e0fcad2d
drkknigt <96606690+drkknigt@users.noreply.github.com>
2022-06-10 21:39:15 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible git
+sudo apt install --yes ansible git
 
 sudo ansible-pull -U https://github.com/drkknigt/ansible_sync -vvv
