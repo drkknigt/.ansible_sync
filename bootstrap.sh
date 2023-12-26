2f79192233198bcd81e11b96e02a7ad8ce88eb46
drkknght <vitobnd79@gmail.com>
2023-12-26 16:59:50 +0530

    refactored config 6

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
 read -p "Do you want to install rtl8821ce wifi driver ? (yes/no)" wifi_info
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install --yes ansible git
+if [ ! "1" -gt "$#" ] ; then
+    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-become-pass -t "$(echo "$@" | tr " " ",")"
+    exit
+fi
 
 if [ "$wifi_info" = "yes" ]; then
     ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-become-pass
