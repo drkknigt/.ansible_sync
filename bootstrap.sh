1224b20d9770ae0072abf04d8d4344509f88d336
drkknght <vitobnd79@gmail.com>
2023-12-29 00:14:08 +0530

    test file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install --yes ansible git
 if [ ! "1" -gt "$#" ] ; then
-    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-become-pass -t "$(echo "$@" | tr " " ",")"
+    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-vault-pass --ask-become-pass -t "$(echo "$@" | tr " " ",")"
     exit
 fi
 
