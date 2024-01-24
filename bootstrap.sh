07c905f274e6f5092470fcffb2df968bae7d2b71
drkknght <vitobnd79@gmail.com>
2024-01-24 11:32:10 +0530

    added ask-pass

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,14 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install --yes ansible git
 if [ ! "1" -gt "$#" ] ; then
-    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-vault-pass --ask-become-pass -t "$(echo "$@" | tr " " ",")"
+    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-vault-pass --ask-become-pass -t "$(echo "$@" | tr " " ",")" --ask-pass
     exit
 fi
 
 if [ "$wifi_info" = "yes" ]; then
-    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-become-pass
+    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-become-pass --ask-pass
 else
-    ansible-pull --extra-vars install_wifi="no" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-become-pass
+    ansible-pull --extra-vars install_wifi="no" -U https://github.com/drkknigt/.ansible_sync -vvv --ask-become-pass --ask-pass
 fi
     
     
